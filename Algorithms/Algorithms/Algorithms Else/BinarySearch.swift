95536b2026435d85fecc2251b5bd994192129c2f
Alex Karamanets <alexander.fromworld@gmail.com>
2023-11-25 08:25:05 +0100

    Algorithms

---
+++ Algorithms/Algorithms/Algorithms Else/BinarySearch.swift
@@ -0,0 +1,8 @@
+//
+//  BinarySearch.swift
+//  Algorithms
+//
+//  Created by Alex Karamanets on 25/11/2023.
+//
+
+import Foundation
---
+++ Algorithms/Algorithms/Source/AlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmsApp.swift
+//  Algorithms
+//
+//  Created by Alex Karamanets on 25/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
